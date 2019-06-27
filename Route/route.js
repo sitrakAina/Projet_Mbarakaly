b5d1e88531b770c8c7be7d516774959c6b80f2e9
PatrickNirina002 <pjsranaivo@gmail.com>
2019-06-27 11:06:32 +0300

    test

--- Route/route.js
+++ Route/route.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express.Router();
 
-var Controller= require('../Controllers/postComment')
-
-app.post('/postComment',Controller.PosteComment)
+var Controller= require('../Controllers/tchat.contolleur')
+app.post('/message', Controller.create);
+    app.get('/message', Controller.findAll);
+    app.get('/profil/:profilId', Controller.findOne);
 
 
 module.exports = app;
