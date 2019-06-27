99a8bd8f39ce981235caf9b1780001513b3f0fb1
RakotoarimalalaNantenaina <naynah344@gmail.com>
2019-06-27 21:28:10 +1000

    ajou frontend

---
+++ front/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Accueil from './components/home'
+import Login from './components/login'
+import { Provider } from 'react-redux'
+import Store from './store/configureStore'
+
+
+function App() {
+  return (
+    <div className="">
+      <Provider store={Store}>
+        <Router>
+          <Route exact path="/" component={Accueil} />
+          <Route path="/login" component={Login} />
+        </Router>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
